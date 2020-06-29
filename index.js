b6042035cc5d674538b3520bdee00b221b8d1edc
SmitnAntonyDv <anton.smit@hotmail.com>
2020-06-29 16:14:57 +0200

    initialize server | added dependencies, config-file, constants.js and skeleton index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const loggerMiddleWare = require("morgan");
+const corseMiddleWare = require("cors");
+const { PORT } = require("./config/constants")
+
+
+//import routers once created
+const app = express();
+
+
+
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+app.use(corseMiddleWare());
+
+if (process.env.DELAY) {
+    app.use((req, res, next) => {
+      setTimeout(() => next(), parseInt(process.env.DELAY));
+    });
+  }
+
+// Routes below!
+
+
+
+
+// listen for connection on port (default port 4000)
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
