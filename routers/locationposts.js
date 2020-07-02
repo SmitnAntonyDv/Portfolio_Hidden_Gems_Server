0ce97643330168078e2dda1420048076de409ac6
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-02 17:39:29 +0200

    added route that retrieves a single post based on PostId

---
+++ routers/locationposts.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const router = new Router();
+
+const User = require("../models").user;
+const Country = reqruie("../models").country;
+const Locationpost = require("../models").locationpost;
+
+router.get("/locationpost/:postId", async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const getPost = await Locationpost.findByPk(postId);
+    if (!getPost) {
+      res.status(400).send("sorry post not found");
+    } else {
+      res.send(getPost);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
