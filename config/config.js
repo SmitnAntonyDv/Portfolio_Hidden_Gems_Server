6f6c011f68171ebff987f2c5855bfd01fb684533
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-10 12:11:25 +0200

    fixed production URL

--- config/config.js
+++ config/config.js
@@ -15,12 +15,12 @@ module.exports = {
     operatorsAliases: '0',
   },
   production: {
+    use_env_variable: "DATABASE_URL",
     username: 'root',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
     dialect: 'postgres',
-    operatorsAliases: '0',
-    use_env_variable: process.env.DATABASE_DEV
+    operatorsAliases: '0'
   },
 };
